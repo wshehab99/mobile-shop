298a3a0fe00feb2c826c2c8a023ecb8bacdf8559
waleed <waleedbinshihab@gmail.com>
2023-11-13 20:39:56 +0300

    top sales

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
     //Banner Owl Carousal
-    $("#banner-area.owl-carousal").owlCarousel({
+    $("#banner-area .owl-carousel").owlCarousel({
         dots: true,
-        items: 1,
-    })
+        items: 1
+    });
+    //Top Sale Carousal
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
 });
